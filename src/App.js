fa3f927a1b6f631025062c5026c0152c44f209aa
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-24 16:46:09 -0500

    button and model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // import React from 'react';
-import React, { Component } from 'react';
-import Hiro from './Components/Hiro/Hiro';
+import { Button, Link } from "@nextui-org/react";
+import React, { Component } from "react";
+import Hiro from "./Components/Hiro/Hiro";
 
 class App extends Component {
   render() {
     return (
+      <>
+        <div>
+          <Link href="https://facebook.com" css={{zIndex:99,position:"absolute",top:"90%",width:"100vw",justifyContent: "center"}}>
+          <Button css={{bgColor: "#c4302b"}}>Go to Music Video</Button>
+          </Link>
+          {/* <a className="button" href="https://facebook.com">
+            Go to
+          </a> */}
+        </div>
         <Hiro />
-      
+      </>
     );
   }
 }
