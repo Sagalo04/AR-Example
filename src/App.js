fb5fd47dccecfabecad64ac733c30f5b0f9a2c6d
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-24 13:20:18 -0500

    resize 3

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-
-import React, { Component } from "react";
-import { AFrameRenderer, Marker } from "react-web-ar";
+// import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+import Hiro from './Components/Hiro/Hiro';
 
 class App extends Component {
   render() {
     return (
-      <AFrameRenderer
-      arToolKit={{ sourceType: 'webcam'}}
-      getSceneRef={ref => (this.scene = ref)} // 
-      inherent={true}
-      areaLearningButton = {false}
-      performanceProfile ={""}
-    
-      >
-        {/* <a-scene> */}
-
-        
-        <Marker parameters={{ preset: "hiro" }}>
-          <a-box
-            color="blue"
-            material="opacity: 1;"
-            position="0 0.09 0"
-            scale="0.4 0.8 0.8"
-          >
-            <a-animation
-              attribute="rotation"
-              to="360 0 0"
-              dur="5000"
-              easing="linear"
-              repeat="indefinite"
-            />
-          </a-box>
-        </Marker>
-        {/* </a-scene> */}
-      </AFrameRenderer>
+      <Hiro />
     );
   }
 }
-
+// render(<App />, document.getElementById('root'));
 export default App;
