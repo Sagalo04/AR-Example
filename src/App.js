afa9986ceb1d1815263e649c5ea0086fa166cc83
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-24 16:52:27 -0500

    fixed button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import React from 'react';
+import { Icon } from "@iconify/react";
 import { Button, Link } from "@nextui-org/react";
 import React, { Component } from "react";
 import Hiro from "./Components/Hiro/Hiro";
@@ -8,8 +9,19 @@ class App extends Component {
     return (
       <>
         <div>
-          <Link href="https://facebook.com" css={{zIndex:99,position:"absolute",top:"90%",width:"100vw",justifyContent: "center"}}>
-          <Button css={{bgColor: "#c4302b"}}>Go to Music Video</Button>
+          <Link
+            href="https://facebook.com"
+            css={{
+              zIndex: 99,
+              position: "absolute",
+              top: "90%",
+              right:"0",
+              width: "100vw",
+              justifyContent: "center",
+            }}
+
+          >
+            <Button css={{ bgColor: "#c4302b" }} icon={<Icon icon="bxs:right-arrow" color="white" height="24" />}>Go to Music Video</Button>
           </Link>
           {/* <a className="button" href="https://facebook.com">
             Go to
