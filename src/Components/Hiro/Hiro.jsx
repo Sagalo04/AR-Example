ece7e5c2f20150f768c1724f8d690a661d61fbdf
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-28 16:33:07 -0500

    nft

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { AFrameRenderer, Marker } from "react-web-ar";
-import patt from "../../pattern-marker.patt";
 
 class Hiro extends Component {
   //   constructor(props) {
@@ -10,54 +9,24 @@ class Hiro extends Component {
 
   render() {
     return (
-      <AFrameRenderer
+      <a-scene
+        vr-mode-ui="enabled: false;"
+        renderer="logarithmicDepthBuffer: true; precision: medium;"
         embedded
-        arjs="debugUIEnabled: false"
-        vr-mode-ui="enabled: false"
+        arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
       >
-        <Marker
-          parameters={{
-            preset: "customs",
-            type: "pattern",
-            url: patt,
-          }}
-          
+        <a-nft
+          type="nft"
+          url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
         >
           <a-entity
             position="0 0 0"
             scale="0.05 0.05 0.05"
             gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
           ></a-entity>
-          <a-animation
-              attribute="rotation"
-              to="360 0 0"
-              dur="5000"
-              easing="linear"
-              repeat="indefinite"
-            />
-          {/* </a-box> */}
-        </Marker>
-        <Marker
-          parameters={{
-            preset: "hiro",
-          }}
-          
-        >
-          <a-entity
-            position="0 0 0"
-            scale="0.05 0.05 0.05"
-            gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-          ></a-entity>
-          <a-animation
-              attribute="rotation"
-              to="360 0 0"
-              dur="5000"
-              easing="linear"
-              repeat="indefinite"
-            />
-          {/* </a-box> */}
-        </Marker>
-      </AFrameRenderer>
+        </a-nft>
+        <a-entity camera></a-entity>
+      </a-scene>
     );
   }
 }
