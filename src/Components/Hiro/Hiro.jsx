8e3a26c6f766b8193012257cc6f9c0e6da5fce28
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-28 15:29:19 -0500

    Agua and Hiro

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { AFrameRenderer, Marker } from "react-web-ar";
-import patt from "../../pat5.patt";
+import patt from "../../pattern-Group.patt";
 
 class Hiro extends Component {
   //   constructor(props) {
@@ -28,13 +28,33 @@ class Hiro extends Component {
             scale="0.05 0.05 0.05"
             gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
           ></a-entity>
-          {/* <a-animation
+          <a-animation
               attribute="rotation"
               to="360 0 0"
               dur="5000"
               easing="linear"
               repeat="indefinite"
-            /> */}
+            />
+          {/* </a-box> */}
+        </Marker>
+        <Marker
+          parameters={{
+            preset: "hiro",
+          }}
+          
+        >
+          <a-entity
+            position="0 0 0"
+            scale="0.05 0.05 0.05"
+            gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+          ></a-entity>
+          <a-animation
+              attribute="rotation"
+              to="360 0 0"
+              dur="5000"
+              easing="linear"
+              repeat="indefinite"
+            />
           {/* </a-box> */}
         </Marker>
       </AFrameRenderer>
