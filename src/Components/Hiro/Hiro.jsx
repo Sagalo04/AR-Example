314f27e986815f612fd33b0f88fdc43243645397
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-28 17:26:25 -0500

    nft example

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -9,24 +9,38 @@ class Hiro extends Component {
 
   render() {
     return (
-      <a-scene
-        vr-mode-ui="enabled: false;"
-        renderer="logarithmicDepthBuffer: true; precision: medium;"
+      <AFrameRenderer
         embedded
-        arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
+        arjs="debugUIEnabled: false"
+        vr-mode-ui="enabled: false"
       >
-        <a-nft
-          type="nft"
-          url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
+        <Marker
+          parameters={{
+            preset: "hiro",
+          }}
         >
           <a-entity
             position="0 0 0"
             scale="0.05 0.05 0.05"
             gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
           ></a-entity>
+        </Marker>
+
+        <a-nft
+          type="nft"
+          url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
+          smooth="true"
+          smoothCount="10"
+          smoothTolerance=".01"
+          smoothThreshold="5"
+        >
+          <a-entity
+            gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+            scale="5 5 5"
+            position="100 100 0"
+          ></a-entity>
         </a-nft>
-        <a-entity camera></a-entity>
-      </a-scene>
+      </AFrameRenderer>
     );
   }
 }
