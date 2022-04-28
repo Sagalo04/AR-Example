5148627d04215117dd4ea4585fb39dd6023446c7
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-28 17:33:52 -0500

    model scale change

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -35,9 +35,9 @@ class Hiro extends Component {
           smoothThreshold="5"
         >
           <a-entity
-            gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-            position="0 0 0"
+            position="5 5 5"
             scale="0.05 0.05 0.05"
+            gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
           ></a-entity>
         </a-nft>
       </AFrameRenderer>
