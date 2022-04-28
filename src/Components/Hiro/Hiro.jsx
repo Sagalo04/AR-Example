36f2ea6043de9badc42b3e2b963adac1c25185ae
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-04-28 17:47:55 -0500

    no smooth

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -33,10 +33,10 @@ class Hiro extends Component {
           <a-nft
             type="nft"
             url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
-            smooth="true"
-            smoothCount="10"
-            smoothTolerance=".01"
-            smoothThreshold="5"
+        // smooth="true"
+        // smoothCount="10"
+        // smoothTolerance=".01"
+        // smoothThreshold="5"
           >
             <a-entity
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
