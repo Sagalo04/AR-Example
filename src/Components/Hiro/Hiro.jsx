4d14f13c79a8a6760bfba17c3b65b55c4f8918ec
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-04 16:43:50 -0500

    cilinder

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -72,18 +72,18 @@ class Hiro extends Component {
             <a-assets-item img id="model1" src={Modelo1} />
             <a-assets-item img id="model2" src={Modelo2} />
 
-            <a-plane
+            <a-entity
               id="model-1"
               model-1-listener
-              // cursor="fuse: true; fuseTimeout: 500"
+              cursor="fuse: true; fuseTimeout: 500"
               position="15 100 0"
               rotation="-90 90 90"
-              // geometry="primitive: box; height: 0.1; depth: 1.5; rotation: 90 0 0;"
-              // material="shader: flat; src: #model1"
-              src="#model1"
+              geometry="primitive: cylinder; height: 0.1; radius: 0.15"
+              material="shader: flat; src: #model1"
+              // src="#model1"
               scale="100 100 100"
             >
-            </a-plane>
+            </a-entity>
 
             {/* <a-entity
               id="model-2"
@@ -95,18 +95,18 @@ class Hiro extends Component {
               scale="100 100 100"
             >
             </a-entity>            */}
-             <a-plane
+             <a-entity
               id="model-2"
               model-2-listener
               rotation="-90 90 90"
-              // cursor="fuse: true; fuseTimeout: 500"
+              cursor="fuse: true; fuseTimeout: 500"
               position="0 0 0"
-              // geometry="primitive: box; height: 0.1; depth: 1.5; rotation: 90 0 0;"
-              // material="shader: flat; src: #model2"
-              src="#model2"
+              geometry="primitive: cylinder; height: 0.1; radius: 0.15"
+              material="shader: flat; src: #model2"
+              // src="#model2"
               scale="100 100 100"
             >
-            </a-plane>
+            </a-entity>
             {/* <a-entity
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
               position="20 75 0"
