635604c86406f98d0be6346077cd2d207d90ab53
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-06 12:39:15 -0500

    postion and scale?

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -109,12 +109,12 @@ class Hiro extends Component {
               gltf-model="../../Modelo/Agua/Agua.gltf"
               position="0 0 0"
               rotation="-90 90 -90"
-              scale="60 1 60"
+              scale="100 100 100"
             ></a-entity>
             <a-entity
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-              position='20 75 0'
-              rotation='-90 -90 -90'
+              position='0 0 0'
+              rotation='-90 90 -90'
               scale='5 5 5'
             ></a-entity>
           </a-nft>
