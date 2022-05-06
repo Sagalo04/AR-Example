6d4d6880c71f9f8ed25608c7f6398bde1cf9231f
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-06 16:29:08 -0500

    one scale

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -117,7 +117,7 @@ class Hiro extends Component {
               gltf-model={URL2}
               position="0 0 0"
               rotation="-90 90 -90"
-              scale="5 1 5"
+              scale="1 1 1"
             ></a-entity>
             <a-entity
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
