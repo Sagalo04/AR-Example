8746f843fd0a3861e22a3263fb76540821da8800
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-06 12:21:48 -0500

    No rotation or scale

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -106,10 +106,10 @@ class Hiro extends Component {
               scale="272 0 272"
             ></a-entity> */}
             <a-entity
-              gltf-model="https://arjs-cors-proxy.herokuapp.com/https://github.com/Sagalo04/AR-Example/blob/master/src/Modelo/Agua/Agua.gltf"
+              gltf-model="../../Modelo/Agua/Agua.gltf"
               position="20 75 0"
-              rotation="-90 90 -90"
-              scale="10 1 10"
+              // rotation="-90 90 -90"
+              // scale="5 1 5"
             ></a-entity>
             <a-entity
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
