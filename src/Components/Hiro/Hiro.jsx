75ef63f428cffd6d718fd3b731ee3af1b5a1922a
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-06 11:51:34 -0500

    no materials new model

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -106,9 +106,7 @@ class Hiro extends Component {
               scale="272 0 272"
             ></a-entity> */}
             <a-entity
-              // gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
               gltf-model="../../Modelo/Agua/Agua.gltf"
-              material="src: #model1"
               position="20 75 0"
               // rotation="-90 90 -90"
               scale="5 0 5"
