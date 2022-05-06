9f045503031dbcaf1df1b300581a9569967aef43
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-05 20:22:36 -0500

    no shader

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -108,7 +108,7 @@ class Hiro extends Component {
             <a-entity
               // gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
               gltf-model="../../Modelo/untitled (1).gltf"
-              material="shader: flat;src: #model1"
+              material="src: #model1"
               position="20 75 0"
               // rotation="-90 90 -90"
               scale="5 0 5"
