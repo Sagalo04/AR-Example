4d541a3695bfd67e7d74f6c0fc9596e948176c7b
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-06 12:15:10 -0500

    Ajustes Agua

--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -109,11 +109,11 @@ class Hiro extends Component {
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://github.com/Sagalo04/AR-Example/blob/master/src/Modelo/Agua/Agua.gltf"
               position="20 75 0"
               rotation="-90 90 -90"
-              scale="1 1 1"
+              scale="10 1 10"
             ></a-entity>
             <a-entity
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-              position='75 75 0'
+              position='20 75 0'
               rotation='-90 -90 -90'
               scale='5 5 5'
             ></a-entity>
