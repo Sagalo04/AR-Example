e011f2c6800ef4f48da9394dc2bbae52d5fde8d1
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-05-09 16:28:56 -0500

    LET SEE

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 // import React from 'react';
-import { Icon } from "@iconify/react";
-import { Button, Link } from "@nextui-org/react";
 import React, { Component } from "react";
 import Hiro from "./Components/Hiro/Hiro";
-import boton from "./AGUA 5.png"
 
 class App extends Component {
   render() {
     return (
       <>
-        <div>
-          <Link
-            href="https://facebook.com"
-            css={{
-              zIndex: 99,
-              position: "absolute",
-              top: "90%",
-              right:"0",
-              width: "100vw",
-              justifyContent: "center",
-            }}
-
-          >
-            <img src={boton} alt="" style={{height:"85px"}}/>
-            {/* <Button css={{ bgColor: "#c4302b" }} icon={<Icon icon="bxs:right-arrow" color="white" height="24" />}>Go to Music Video</Button> */}
-          </Link>
-          {/* <a className="button" href="https://facebook.com">
-            Go to
-          </a> */}
-        </div>
         <Hiro />
       </>
     );
--- src/Components/Hiro/Hiro.jsx
+++ src/Components/Hiro/Hiro.jsx
@@ -1,26 +1,51 @@
-import { Loading } from "@nextui-org/react";
+import { Link, Loading } from "@nextui-org/react";
 import React, { Component } from "react";
-import { AFrameRenderer, } from "react-web-ar";
+import { AFrameRenderer } from "react-web-ar";
 import "./Hiro.css";
+import boton from "../../AGUA 5.png";
 
 class Hiro extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.myRef = React.createRef();
-  //   }
-
-  
+  constructor(props) {
+    super(props);
+    this.state = { btn: false };
+  }
 
   render() {
-    
     const URL = process.env.PUBLIC_URL + "agua/agua";
     const URL2 = process.env.PUBLIC_URL + "Modelo/finalscene.glb";
+
+    // eslint-disable-next-line no-undef
+    AFRAME.registerComponent("markerhandler", {
+      init: function () {
+        this.el.sceneEl.addEventListener("markerFound", () => {
+          console.log("markerFound");
+         document.getElementById("linkk").className = "visibilidad";
+        });
+        this.el.sceneEl.addEventListener("markerLost", () => {
+         document.getElementById("linkk").className = "visibilidadNo";
+         console.log("markerLost");
+        });
+      },
+    });
+
     return (
       <>
-      {
-        // eslint-disable-next-line no-undef
-
-      }
+        <Link
+          href="https://facebook.com"
+          className="visibilidadNo"
+          // css={{
+          //   zIndex: 99,
+          //   position: "absolute",
+          //   top: "90%",
+          //   right: "0",
+          //   width: "100vw",
+          //   justifyContent: "center",
+          // }}
+          id="linkk"
+        >
+          <img src={boton} alt="" style={{ height: "75px" }} />
+          {/* <Button css={{ bgColor: "#c4302b" }} icon={<Icon icon="bxs:right-arrow" color="white" height="24" />}>Go to Music Video</Button> */}
+        </Link>
         <div className="arjs-loader">
           <div>
             <Loading type="points" />
@@ -30,10 +55,11 @@ class Hiro extends Component {
           vr-mode-ui="enabled: false;"
           renderer="logarithmicDepthBuffer: true;"
           embedded
-          getSceneRef={ref => (this.scene = ref)}
+          hello-world
           arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
         >
           <a-nft
+            ref={this.myRef}
             markerhandler
             type="nft"
             url={URL}
@@ -44,14 +70,13 @@ class Hiro extends Component {
           >
             <a-entity
               gltf-model={URL2}
-              position="50 25 0"
+              position="300 0 0"
               rotation="-90 90 -90"
               scale="0.5 0.5 0.5"
             ></a-entity>
-
           </a-nft>
 
-          <a-entity camera></a-entity>
+          {/* <a-entity camera></a-entity> */}
         </AFrameRenderer>
       </>
     );
